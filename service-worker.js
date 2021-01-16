4cf54fd8f0b5d144153946bcfac06883cfd30c21
Scenaristeur <scenaristeur@gmail.com>
2021-01-16 17:33:44 +0100

    add kanban

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/ess-table/precache-manifest.7f20c64e2c453dd077536c42a26eaf9a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/ess-table/precache-manifest.e18e606316c042a789cac21483c99c5b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 // src/sw.js
 // https://auth0.com/blog/vuejs-kanban-board-adding-progressive-web-app-features/#An-Introduction-to-Service-Workers
