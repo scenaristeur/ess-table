50da981d760e509bf9891e26c119265e5ac934de
Scenaristeur <scenaristeur@gmail.com>
2021-01-18 03:17:15 +0100

    fix bases url

---
+++ precache-manifest.176bb3219a4299aa3b5e2cbf411236fc.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce0995ef1f3ca20c1d2287e1488a8902",
+    "url": "/ess-table/404.html"
+  },
+  {
+    "revision": "7021180ccca8ab94068b",
+    "url": "/ess-table/css/app.0d554beb.css"
+  },
+  {
+    "revision": "41c30cc3c2e4b2181055",
+    "url": "/ess-table/css/bases.5975915a.css"
+  },
+  {
+    "revision": "f88b648a5e2e334772e3",
+    "url": "/ess-table/css/chunk-0e88118b.4ee9f3c0.css"
+  },
+  {
+    "revision": "939953bb1b7a0dbd1883",
+    "url": "/ess-table/css/chunk-33fa39da.40424b30.css"
+  },
+  {
+    "revision": "5b688eeff1fb58d2b201",
+    "url": "/ess-table/css/chunk-8cbc85f4.6773d80b.css"
+  },
+  {
+    "revision": "883df573345ff29fe2e2",
+    "url": "/ess-table/css/chunk-b22309ce.44553381.css"
+  },
+  {
+    "revision": "11c37316dfed35c969bb",
+    "url": "/ess-table/css/chunk-vendors.b817d239.css"
+  },
+  {
+    "revision": "0f2823211d9e63ed72e8911daf5d4b76",
+    "url": "/ess-table/index.html"
+  },
+  {
+    "revision": "83f75edb96b4cbbda2af",
+    "url": "/ess-table/js/about.8e4e4a3d.js"
+  },
+  {
+    "revision": "8ef735c10cd281a5acdc",
+    "url": "/ess-table/js/activity.db0c7c97.js"
+  },
+  {
+    "revision": "7021180ccca8ab94068b",
+    "url": "/ess-table/js/app.4a6861bc.js"
+  },
+  {
+    "revision": "5e32fb99b74f0c0ba726",
+    "url": "/ess-table/js/backlog.440eda47.js"
+  },
+  {
+    "revision": "41c30cc3c2e4b2181055",
+    "url": "/ess-table/js/bases.a1f80fca.js"
+  },
+  {
+    "revision": "f88b648a5e2e334772e3",
+    "url": "/ess-table/js/chunk-0e88118b.4cee9581.js"
+  },
+  {
+    "revision": "a87e84c2601849296fe8",
+    "url": "/ess-table/js/chunk-2b385736.4495e03a.js"
+  },
+  {
+    "revision": "89fd97d7e2382b34c195",
+    "url": "/ess-table/js/chunk-2d20fb75.0f189ced.js"
+  },
+  {
+    "revision": "dde4a386224c7bfa36a8",
+    "url": "/ess-table/js/chunk-2d226c5a.61e9e934.js"
+  },
+  {
+    "revision": "84b6dfd3f41605514532",
+    "url": "/ess-table/js/chunk-3021a89c.5fea540c.js"
+  },
+  {
+    "revision": "939953bb1b7a0dbd1883",
+    "url": "/ess-table/js/chunk-33fa39da.61969f7c.js"
+  },
+  {
+    "revision": "cbe6eb0376a331c61b81",
+    "url": "/ess-table/js/chunk-4a5f46a6.d9c728ec.js"
+  },
+  {
+    "revision": "5b688eeff1fb58d2b201",
+    "url": "/ess-table/js/chunk-8cbc85f4.e1f257bf.js"
+  },
+  {
+    "revision": "883df573345ff29fe2e2",
+    "url": "/ess-table/js/chunk-b22309ce.9dfa8566.js"
+  },
+  {
+    "revision": "a1d6c99a7f0834bc4a65",
+    "url": "/ess-table/js/chunk-b7902c50.d6e76f6f.js"
+  },
+  {
+    "revision": "11c37316dfed35c969bb",
+    "url": "/ess-table/js/chunk-vendors.b3a8cae9.js"
+  },
+  {
+    "revision": "e4ee102aa942b3b41c7f",
+    "url": "/ess-table/js/kanban.f64b60fb.js"
+  },
+  {
+    "revision": "00f940cd17844bfcf847",
+    "url": "/ess-table/js/tablesviews.61335620.js"
+  },
+  {
+    "revision": "5bdb8963bf5f0701e43f",
+    "url": "/ess-table/js/templates.5afa013d.js"
+  },
+  {
+    "revision": "a9da1dc2eee81e2fbbea",
+    "url": "/ess-table/js/workspaces.2e850582.js"
+  },
+  {
+    "revision": "068fc75e7e0b089cd746bab7cc4d8d87",
+    "url": "/ess-table/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/ess-table/robots.txt"
+  }
+]);
