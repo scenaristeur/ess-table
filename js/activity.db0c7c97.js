7890ec5e111453abae31675e13e6fc9938a349fb
Smag0 <scenaristeur@gmail.com>
2021-02-03 20:25:12 +0100

    catch url

--- js/activity.db0c7c97.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["activity"],{abae:function(t,e,n){"use strict";n.r(e);var i=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("b-container",[t._v(" Activity is not totally implemented yet ")])},a=[],c={name:"Activity"},l=c,o=n("2877"),r=Object(o["a"])(l,i,a,!1,null,null,null);e["default"]=r.exports}}]);
-//# sourceMappingURL=activity.db0c7c97.js.map
